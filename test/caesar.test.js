60a168d8f8403c7b9c2a09f14ff060cd94e78596
brianwmorgan <brianwmorgan317@gmail.com>
2022-12-08 12:03:36 -0500

    added updated version of unit tests for caesar() function; commented out older version of unit tests for caesar() function

--- test/caesar.test.js
+++ test/caesar.test.js
@@ -1,3 +1,70 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// VERSION #2 - DECEMBER 2022
+
+const expect = require("chai").expect;
+const { caesar } = require("../src/caesar.js");
+
+describe("caesar()", () => {
+  it("should return 'false' if the shift value is equal to '0'", () => {
+    const input = "Zebra Magazine";
+    const shift = 0;
+    const actual = caesar(input, shift);
+    expect(actual).to.be.false;
+  });
+  it("should return 'false' if the shift value is less than '-25'", () => {
+    const input = "Zebra Magazine";
+    const shift = -26;
+    const actual = caesar(input, shift);
+    expect(actual).to.be.false;
+  });
+  it("should return 'false' if the shift value is greater than '25'", () => {
+    const input = "Zebra Magazine";
+    const shift = 26;
+    const actual = caesar(input, shift);
+    expect(actual).to.be.false;
+  });
+  it("should return 'false' if the shift value is not present", () => {
+    const input = "Zebra Magazine";
+    const shift = null;
+    const actual = caesar(input, shift);
+    expect(actual).to.be.false;
+  });
+  it("should ignore capital letters", () => {
+    const input = "Zebra Magazine";
+    const shift = 3;
+    const expected = "cheud pdjdclqh";
+    const actual = caesar(input, shift);
+    expect(actual).to.equal(expected);
+  });
+  it("should handle shifts that go past the beginning of the alphabet", () => {
+    const input = "abc";
+    const shift = -3;
+    const expected = "xyz";
+    const actual = caesar(input, shift);
+    expect(actual).to.equal(expected);
+  });
+  it("should handle shifts that go past the end of the alphabet", () => {
+    const input = "xyz";
+    const shift = 3;
+    const expected = "abc";
+    const actual = caesar(input, shift);
+    expect(actual).to.equal(expected);
+  });
+  it("should maintain spaces and other nonalphabetic symbols", () => {
+    const input = "123 456 789 Zebra Magazine !@# $%^ &*(";
+    const shift = 3;
+    const expected = "123 456 789 cheud pdjdclqh !@# $%^ &*(";
+    const actual = caesar(input, shift);
+    expect(actual).to.equal(expected);
+  });
+});
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// VERSION #1 - FEBRUARY 2022
+
+/*
 const expect = require("chai").expect;
 const { caesar } = require("../src/caesar");
 
@@ -82,3 +149,6 @@ describe("caesar", () => {
       });
 
 });
+
+*/
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
