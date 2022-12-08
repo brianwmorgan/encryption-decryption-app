5ecb40eb7fecb7b5eb37a0d4896e8ffdaa592ad0
brianwmorgan <brianwmorgan317@gmail.com>
2022-12-08 12:25:11 -0500

    added updated version of unit tests for substitution() function ; commented out older version of unit tests for substitution() function

--- test/substitution.test.js
+++ test/substitution.test.js
@@ -1,3 +1,59 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// VERSION #2 - DECEMBER 2022
+
+const expect = require("chai").expect;
+const { substitution } = require("../src/substitution.js");
+
+describe("substitution()", () => {
+  it("should return 'false' if the given 'alphabet' isn't exactly 26 characters long", () => {
+    const input = "hello world";
+    const alphabet = "abcdefghijklmnop";
+    const actual = substitution(input, alphabet);
+    expect(actual).to.be.false;
+  });
+  it("should correctly encode the given phrase", () => {
+    const input = "message";
+    const alphabet = "$wae&zrdxtfcygvuhbijnokmpl";
+    const expected = "y&ii$r&"
+    const actual = substitution(input, alphabet);
+    expect(actual).to.equal(expected);
+  });
+  it("should correctly decode the given phrase", () => {
+    const input = "y&ii$r&";
+    const alphabet = "$wae&zrdxtfcygvuhbijnokmpl";
+    const encode = false;
+    const expected = "message"
+    const actual = substitution(input, alphabet, encode);
+    expect(actual).to.equal(expected);
+  });
+  it("should return 'false' if the given 'alphabet' contains any duplicate characters", () => {
+    const input = "hello world";
+    const alphabet = "abcdddghijklmopqrs222wxyz";
+    const actual = substitution(input, alphabet);
+    expect(actual).to.be.false;
+  });
+  it("should maintain spaces in the message", () => {
+    const input = "m e s s a g e";
+    const alphabet = "$wae&zrdxtfcygvuhbijnokmpl";
+    const expected = "y & i i $ r &"
+    const actual = substitution(input, alphabet);
+    expect(actual).to.equal(expected);
+  });
+  it("should ignore capital letters", () => {
+    const input = "MESSAGE";
+    const alphabet = "$wae&zrdxtfcygvuhbijnokmpl";
+    const expected = "y&ii$r&"
+    const actual = substitution(input, alphabet);
+    expect(actual).to.equal(expected);
+  });
+});
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// VERSION #1 - FEBRUARY 2022
+
+/*
 const { expect } = require("chai");
 const { substitution } = require("../src/substitution");
 
@@ -81,3 +137,6 @@ describe("substitution", () => {
     });
 
 });
+*/
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
