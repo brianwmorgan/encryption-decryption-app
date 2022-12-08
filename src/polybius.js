0eb0a5160edfbd14dfacdc2b4b50ae56999bea41
brianwmorgan <brianwmorgan317@gmail.com>
2022-12-08 12:09:17 -0500

    added updated version of polybius() function ; commented out older version of polybius() function

--- src/polybius.js
+++ src/polybius.js
@@ -1,3 +1,69 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// VERSION #2 - DECEMBER 2022
+
+const polybiusModule = (function () {
+
+  function polybius(input, encode = true) {
+
+    const encoder = {
+      a: "11", b: "21", c: "31", d: "41", e: "51",
+      f: "12", g: "22", h: "32", i: "42", j: "42", k: "52",
+      l: "13", m: "23", n: "33", o: "43", p: "53",
+      q: "14", r: "24", s: "34", t: "44", u: "54",
+      v: "15", w: "25", x: "35", y: "45", z: "55",
+    }
+    const decoder = {
+      11: "a", 21: "b", 31: "c", 41: "d", 51: "e",
+      12: "f", 22: "g", 32: "h", 42: "(i/j)", 52: "k",
+      13: "l", 23: "m", 33: "n", 43: "o", 53: "p",
+      14: "q", 24: "r", 34: "s", 44: "t", 54: "u",
+      15: "v", 25: "w", 35: "x", 45: "y", 55: "z",
+    }
+
+    if (encode) {
+      let message = input.toLowerCase();
+      let result = "";
+      
+      for (let i = 0; i < message.length; i++) {
+        let character = message[i];
+        if (encoder[character]) {
+          result += encoder[character];
+        } else {
+          result += character;
+        };
+      }
+
+      return result;
+
+    } else {
+      let inputWithoutSpaces = input.split(" ").join("");
+      if (!Number.isInteger(inputWithoutSpaces.length/2)) return false;
+
+      let result = "";
+      for (let i = 0; i < input.length; i += 2) {
+        let code = `${input[i]}${input[i + 1]}`;
+        if (code.includes(" ")) {
+          result += " ";
+          i--;
+        } else {
+          result += decoder[code];
+        };
+      }
+      return result;
+    }
+  }
+
+  return {
+    polybius,
+  };
+})();
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// VERSION #1 - FEBRUARY 2022
+
+/*
 const polybiusModule = (function () {
 
   function polybius(input, encode = true) {
@@ -65,6 +131,8 @@ const polybiusModule = (function () {
     polybius,
   };
 })();
+*/
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 module.exports = { polybius: polybiusModule.polybius };
